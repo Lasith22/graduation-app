00be614362206a11ebaac1e4cb026a2d12b6f2e1
Lasith Herath <81031378+Lasith22@users.noreply.github.com>
2025-03-09 08:01:53 +0530

    Add MainForm component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import SimpleFirestoreButton from './components/ SimpleFirestoreButton';
+import MainForm from './components/MainForm';
 
 function App() {
   return (
     <>
-      <div className=" min-h-screen bg-[#F7F9FA]">
-        <h1 className=" text-amber-300">helo</h1>
+      <div className=" min-h-screen bg-[#F7F9FA] p-[30px]">
+        <MainForm />
       </div>
     </>
   );
