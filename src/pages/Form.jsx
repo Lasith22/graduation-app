82c0951df3b4a6316db1175a271ad8f9e8deb7b7
Lasith Herath <81031378+Lasith22@users.noreply.github.com>
2025-03-10 13:09:36 +0530

    Integrate Footer component into Form page layout

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
 import MainForm from '../components/form/MainForm';
+import Footer from '../components/home/Footer';
 
 const Form = () => {
   return (
     <>
       <div className="bg-[#F3F3F3] p-5 md:p-20">
         <MainForm />
+        <Footer />
       </div>
     </>
   );
