67e5903bea93e43b34a5670decb1ac8069acc090
Lasith Herath <81031378+Lasith22@users.noreply.github.com>
2025-03-10 06:22:15 +0530

    Update IntroDuction component with Sinhala translations and improved layout

--- src/components/home/IntroDuction.jsx
+++ src/components/home/IntroDuction.jsx
@@ -3,21 +3,26 @@ import React from 'react';
 const IntroDuction = () => {
   return (
     <div className="bg-white p-[30px]">
-      <div className="flex flex-col gap-10 justify-center items-center ">
+      <div className="flex flex-col gap-5 md:gap-10 justify-center items-center ">
         <h1 className="text-xl md:text-3xl  font-bold text-center font-primary">
           {' '}
-          Dr.Hayeshika Fernando 2024 - Scholarship Exam Award Photo Registration
-          Form
+          Dr.Hayeshika Fernando 2024 - ශිෂ්‍යත්ව විභාග සම්මාන ඡායාරූප ලබා ගැනීමේ
+          නාම ලේඛනය
         </h1>
-        <h1 className=" text-sm md:text-xl text-center font-primary font-medium leading-normal tracking-tight-[1.3px]">
-          You can get your child's photo framed here and also individually. This
-          photo is similar to a birth photo and you will receive the photo on
-          the same day, students who want to get this photo should come to the
-          photo taking location with their parents two hours before the event
-          starts, more than 8,000 students will participate on that day and we
-          expect your support. Complete this application only if you are getting
-          the photo. Thank you!
-        </h1>
+        <div className="flex flex-col gap-2 md:gap-4 ">
+          <h1 className="text-sm md:text-xl text-start font-primary font-light leading-normal tracking-tight-[1.3px]">
+            ඔබට මෙහිදී තමන්ගේ දරුවා සමග සහ ඔවුන්ගේ ඡායාරූපය තනි තනි වශයෙන් ද
+            රාමුකර ලබාගත හැක. මෙම ඡායාරූපය උපාදී ඡායාරූපයකට සමාන වන අතර එදින
+            පැමිණෙන ඔබට ඡායාරූපය එදිනම ලැබෙනු ඇත, මෙම ඡායාරූපය ලබා ගැනීමට අවශ්‍ය
+            සිසුන් දෙමවුපියන් සමග උත්සවය ආරම්භ කිරීමට පැය දෙකකට පෙර ඡායාරූප
+            ගන්නා ස්ථානය වෙත පැමිණෙන්න, එදින ලමුන් 8,000 ට අධික සිසුන් මේ සදහා
+            සහභාගී වන අතර ඔබගේ සහයෝගය අප බලාපොරොත්තු වෙමු.
+          </h1>
+          <h1 className="text-sm md:text-xl text-start font-primary font-bold leading-normal tracking-tight-[1.3px]">
+            ඔබ ඡායාරූපය ලබාගන්නවානම් පමනක් මෙම අයදුම්පත්‍රය සම්පූර්ණ කරන්න.
+            ස්තූතී!
+          </h1>
+        </div>
       </div>
     </div>
   );
