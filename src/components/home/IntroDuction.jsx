03fdef8ef21ed682d043af782963e82d27fff9c7
Lasith Herath <81031378+Lasith22@users.noreply.github.com>
2025-03-10 06:03:56 +0530

    Update IntroDuction component with additional description and responsive heading styles

--- src/components/home/IntroDuction.jsx
+++ src/components/home/IntroDuction.jsx
@@ -3,11 +3,20 @@ import React from 'react';
 const IntroDuction = () => {
   return (
     <div className="bg-white p-[30px]">
-      <div className="flex justify-center items-center ">
-        <h1 className="text-3xl font-bold text-center font-primary">
+      <div className="flex flex-col gap-10 justify-center items-center ">
+        <h1 className="text-xl md:text-3xl  font-bold text-center font-primary">
           {' '}
           Dr.Hayeshika Fernando 2024 - Scholarship Exam Award Photo Registration
           Form
+        </h1>
+        <h1 className=" text-sm md:text-xl text-center font-primary font-medium leading-normal tracking-tight-[1.3px]">
+          You can get your child's photo framed here and also individually. This
+          photo is similar to a birth photo and you will receive the photo on
+          the same day, students who want to get this photo should come to the
+          photo taking location with their parents two hours before the event
+          starts, more than 8,000 students will participate on that day and we
+          expect your support. Complete this application only if you are getting
+          the photo. Thank you!
         </h1>
       </div>
     </div>
