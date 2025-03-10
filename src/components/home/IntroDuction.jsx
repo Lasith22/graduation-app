85d0a5745d4ba66c89282fa1eeb70e94f5cbffde
Lasith Herath <81031378+Lasith22@users.noreply.github.com>
2025-03-10 06:27:10 +0530

    Update IntroDuction component styles for improved readability and aesthetics

--- src/components/home/IntroDuction.jsx
+++ src/components/home/IntroDuction.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IntroDuction = () => {
   return (
-    <div className="bg-white p-[30px]">
+    <div className="bg-[#F3F3F3] p-[30px]">
       <div className="flex flex-col gap-5 md:gap-10 justify-center items-center ">
         <h1 className="text-xl md:text-3xl  font-bold text-center font-primary">
           {' '}
@@ -10,7 +10,7 @@ const IntroDuction = () => {
           නාම ලේඛනය
         </h1>
         <div className="flex flex-col gap-2 md:gap-4 ">
-          <h1 className="text-sm md:text-xl text-start font-primary font-light leading-normal tracking-tight-[1.3px]">
+          <h1 className="text-sm md:text-xl text-start font-primary font-light text-gray-700 leading-normal tracking-tight-[1.3px]">
             ඔබට මෙහිදී තමන්ගේ දරුවා සමග සහ ඔවුන්ගේ ඡායාරූපය තනි තනි වශයෙන් ද
             රාමුකර ලබාගත හැක. මෙම ඡායාරූපය උපාදී ඡායාරූපයකට සමාන වන අතර එදින
             පැමිණෙන ඔබට ඡායාරූපය එදිනම ලැබෙනු ඇත, මෙම ඡායාරූපය ලබා ගැනීමට අවශ්‍ය
@@ -18,7 +18,7 @@ const IntroDuction = () => {
             ගන්නා ස්ථානය වෙත පැමිණෙන්න, එදින ලමුන් 8,000 ට අධික සිසුන් මේ සදහා
             සහභාගී වන අතර ඔබගේ සහයෝගය අප බලාපොරොත්තු වෙමු.
           </h1>
-          <h1 className="text-sm md:text-xl text-start font-primary font-bold leading-normal tracking-tight-[1.3px]">
+          <h1 className="text-sm md:text-xl text-start font-primary font-bold text-gray-700 leading-normal tracking-tight-[1.3px]">
             ඔබ ඡායාරූපය ලබාගන්නවානම් පමනක් මෙම අයදුම්පත්‍රය සම්පූර්ණ කරන්න.
             ස්තූතී!
           </h1>
