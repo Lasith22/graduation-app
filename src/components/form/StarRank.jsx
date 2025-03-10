e739e0577e6cb1be9684c74751b06b13dd714ed7
Lasith Herath <81031378+Lasith22@users.noreply.github.com>
2025-03-10 11:44:44 +0530

    Add star rank images and update StarRank component with localized labels and layout improvements

--- src/components/form/StarRank.jsx
+++ src/components/form/StarRank.jsx
@@ -3,12 +3,12 @@ import { Radio, Space } from 'antd';
 
 const StarRank = () => {
   return (
-    <div className="p-6 bg-white rounded-lg shadow-sm">
+    <div className="">
       {/* Date Selection */}
-      <div className="mb-6">
+      <div className="mb-6 font-primary">
         <label className="block text-gray-800 font-medium mb-2">
-          Date you will be attending the event{' '}
-          <span className="text-red-500">*</span>
+          ඔබ උත්සවයට සහභාගීවන දවස
+          <span className="text-red-500 mx-1 text-2xl">*</span>
         </label>
         <Radio.Group className="w-full">
           <Space direction="vertical" className="w-full">
@@ -27,29 +27,34 @@ const StarRank = () => {
       </div>
 
       {/* Time Selection */}
-      <div className="mb-6">
+      <div className="mb-6 font-primary">
         <label className="block text-gray-800 font-medium mb-2">
-          When you will be attending the event{' '}
-          <span className="text-red-500">*</span>
+          ඔබ උත්සවයට සහභාගීවන වේලාව
+          <span className="text-red-500 mx-1 text-2xl">*</span>
         </label>
         <p className="text-sm text-gray-600 mb-2">
-          Students who wish to take this photo should arrive at the photo
-          location two hours before their scheduled time.
+          මෙම ඡායාරූපය ලබා ගැනීමට අවශ්‍යෙ සිසුන් දෙමවුපියන් සමග උත්සවය ආරම්බ
+          කිරීමට පැය දෙකකට පෙර ඡායාරූප ගන්නා ස්ථානය වෙත පැමිණෙන්න,
         </p>
         <Radio.Group className="w-full">
           <Space direction="vertical" className="w-full">
             <Radio value="morning">
               <div className="py-1 px-3 bg-gray-100 rounded text-gray-700 inline-block">
-                9:00 am to 11:30 pm (If you arrive at 6:30 a.m.)
+                9:00 am to 11:30 pm (එසේනම් ඔබ උදෑසන 6.30 පැමිණෙන්න.)
               </div>
             </Radio>
             <Radio value="afternoon">
               <div className="py-1 px-3 bg-gray-100 rounded text-gray-700 inline-block">
-                3:00 pm to 5:30 pm (If you arrive at 12:30 p.m.)
+                3:00 pm to 5:30 pm (එසේනම් ඔබ දහවල් 12.30 පැමිණෙන්න.)
               </div>
             </Radio>
           </Space>
         </Radio.Group>
+      </div>
+
+      <div className="flex justify-start   items-center">
+        <h1 className=" text-2xl">Stars Rank</h1>{' '}
+        <span className="text-red-500 mx-1 text-2xl">*</span>
       </div>
     </div>
   );
